7059308dcc8772f49270f5938fbdde3d46452e2b
Mark Shapland <mark_shapland@hotmail.com>
2021-04-05 12:44:37 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+var X = "Hello, World!" //assigning a variable
+window.alert (X); //Utilizing the window alert method
+var Daughter1 = "Viviana is my first daughter.", Daughter2 = " Giovana is my second daughter."; //assigning variables
+var Daughter1 = Daughter1.fontcolor("blue"), Daughter2 = Daughter2.fontcolor("green"); //assigning font colors to variables.
+document.write(Daughter1 + Daughter2); //utilizing the document write method and concanating the values of two str variables.
+document.write( 55 * 74.3); //expression
