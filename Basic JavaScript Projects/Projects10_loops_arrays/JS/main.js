cf4e58f01da61ecf18e06f16287fb7aedaa857cc
Mark Shapland <mark_shapland@hotmail.com>
2021-04-13 10:56:39 -0700

    PROJECT10_LOOPS_ARRAYS SUBMISSION ASSIGNMENT
    PROJECT10_LOOPS_ARRAYS SUBMISSION ASSIGNMENT

--- Basic JavaScript Projects/Projects10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Projects10_loops_arrays/JS/main.js
@@ -16,10 +16,10 @@ function String() {
 }
 //For Loop
 var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
-var Content = "";
-var Y; 
-function for_Loop() { 
-    for (Y = 0; Y < Instruments.length;  Y++) {
+    var Content = "";
+    var Y; 
+    function for_Loop() { 
+        for (Y = 0; Y < Instruments.length;  Y++) {
         Content += Instruments[Y] + "<br>";
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
@@ -42,13 +42,22 @@ function constant_function() {
     + "<br>" + Cups.brand + " cups.";
 }
 
+ // let keyword with properties
+ let clock = { Type: "analog,  ", Color: "dark espresso, ", Shape:"round, ", Frame_Material: "plastic ",
+ Description : function() {
+  return "The clock is an " + this.Type + this.Shape + this.Color + " clock, made from " +  this.Frame_Material + ".";
+ }
+};
+document.getElementById("Clock").innerHTML = clock.Description(); 
+
+
 // let keyword
 {
-let a = 22;
-    document.write(a);
+let f = 22;
+    document.write(f);
 }
 
-// 
+// return statement
 var d = myFunction(5, 37); 
 
 function myFunction(a, b) {
@@ -56,8 +65,23 @@ function myFunction(a, b) {
 }  
 document.getElementById("function").innerHTML = d;
 
-  
+//loop with break
+var loop_break = "";
+var i;
+for (i = 0; i < 10; i++) {
+    if (i === 5) { break;}
+    loop_break += "The number is " + i + "<br>";
+}
+document.getElementById("Loop_Break").innerHTML = loop_break;
 
+//loop with continue
+var loop_continue = "";
+var j
+for (j = 0; j < 10; j++) {
+    if (j === 5) {continue;}
+    loop_continue += "The number is  " + j + "<br>";
+}
+document.getElementById("Loop_Continue").innerHTML = loop_continue;
 
 
 
